73083ffed428f1cf46bbda568207aaec899b1378
Nikita Kostash <nikitaakostash@gmail.com>
2019-08-12 00:13:17 +0300

    list ready

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import APIVideoSearch from "../node_modules/youtube-api-search";
 import SearchField from "./components/search-field";
 import VideoPlayer from "./components/video-player";
+import SearchVideosList from "./components/search-videos-list/index"
 
 
-const KEY = "AIzaSyDCQNGiEwhpkkkAtq_H9RbSCHtnLjw-QmY"
+const KEY = "AIzaSyCYPJzhrComrCHnKlNtN4iSqYgLfMDR3gQ"
 
 class App extends React.Component {
 
-  
+
   constructor(props) {
     super(props);
     this.state = {
@@ -20,7 +21,7 @@ class App extends React.Component {
   }
 
   videoSearch(term) {
-    APIVideoSearch({key: KEY, term: term, type: "video"}, data => {
+    APIVideoSearch({ key: KEY, term: term, type: "video" }, data => {
       this.setState({
         videos: data,
         selected: data[0]
@@ -28,15 +29,20 @@ class App extends React.Component {
     });
   }
 
-  videoPlayed = () => {
-    const videoId = this.state.selected.id.videoId;
+
+  yo = () => {
+    console.log("yo")
   }
   render() {
 
     return (
       <div>
-        <SearchField onSearchChange = {(term) => this.videoSearch(term)} />
-        <VideoPlayer video={this.state.selected} />
+        <SearchField onSearchChange={(term) => this.videoSearch(term)} />
+        <div className="row">
+          <SearchVideosList onSelect={selected => this.setState({ selected })}
+            videos={this.state.videos} />
+          <VideoPlayer video={this.state.selected} />
+        </div>
       </div>
     )
   }
